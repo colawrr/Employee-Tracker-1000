d45d8f863cfc06334e49114545df232458ee7f70
Connor Lawrence <connor.lawrence96@gmail.com>
2021-12-07 18:18:35 -0700

    finally connected to db

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(9,2) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
@@ -27,3 +27,4 @@ CREATE TABLE employee (
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
+
